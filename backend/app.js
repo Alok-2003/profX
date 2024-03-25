3f5471f93cf731ece7f4aa305bd6095c1c672841
Alok kumar yadav <alok953280@gmail.com>
2024-03-25 13:16:42 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
+    // origin: [process.env.FRONTEND_URL],
+    origin: ["https://profX.vercel.app/"],
     methods: ["POST"],
     credentials: true,
   })
